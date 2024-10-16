88e037fd8850217bae2f57f733369a792f8bdd5b
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-10-16 14:43:08 +0400

    Coding is cool

---
+++ Excercise 1/Cooding is cool.py
@@ -0,0 +1,12 @@
+#Selecting the word1 to be coding
+word1 = "coding"
+
+#Selecting the word2 to be is
+word2 = "is"
+
+#Selecting the word3 to be cool
+word3 = "cool"
+
+#combining all the words
+c = word1 + " " + word2 + " " + word3
+print (c)
