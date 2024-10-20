6b2db10a702502352f28d50101ff7156fe2ef537
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-10-20 23:09:27 +0400

    simple sum

---
+++ excercise 2/simplesum.py
@@ -0,0 +1,17 @@
+### Steps:
+#1. Declare a variable and initialize it with the integer value `8`.
+#2. Declare a second variable and initialize it with the integer value `10`.
+#3. Declare a third variable that stores the sum of first two numbers.
+#4. Print the value of the sum to the console.
+
+#assigning an integer to the first variable 
+apple = (int(8))
+
+#assigning an integer to the second variable 
+samsung = (int(10))
+
+#Adding both the first and second variable to the third variable
+huawei = (apple + samsung)
+
+#print the variable huawei
+print (huawei)
