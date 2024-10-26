79a20d3f8372ddaa8b8581132d13d2641560a633
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-10-26 13:44:19 +0400

    Primitive Quiz Standard

---
+++ Excercise 4/Primitive quiz standard.py
@@ -0,0 +1,85 @@
+### Advanced Requirements:
+#Ignore Capitalization: Modify your program to accept answers regardless of the capitalization (e.g., "paris", "Paris", and "PaRis" should all be considered correct).
+#Multiple Questions: Extend the program into a quiz that asks for the capitals of 10 European countries. Provide feedback for each question.
+
+#Alloting an introduction to print.
+print("Welcome to the European Capitals Quiz")
+
+#Assigning the first question to the variable.
+Answer = input("What is the capital of France?:")
+if Answer.strip().lower() == "paris" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as Paris.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than Paris.
+
+
+#Assigning the second question to the variable.
+Answer = input("What is the capital of Switzerland?:")
+if Answer.strip().lower() == "bern" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as Bern.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than bern.
+
+
+#Assigning the third question to the variable.
+    Answer = input("What is the capital of Germany?:")
+if Answer.strip().lower() == "berlin" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as Berlin.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than berlin.
+
+
+#Assigning the fourth question to the variable.
+Answer = input("What is the capital of Romania?:")
+if Answer.strip().lower() == "bucharest" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as bucharest.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than bucharest.
+
+
+#Assigning the fifth question to the variable.
+Answer = input("What is the capital of Belgium?:")
+if Answer.strip().lower() == "brussels" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as Brussels.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than Brussels.
+
+
+#Assigning the sixth question to the variable.
+Answer = input("What is the capital of Ireland?:")
+if Answer.strip().lower() == "dublin" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as dublin.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than dublin. 
+
+
+#Assigning the seventh question to the variable.
+Answer = input("What is the capital of Hungary?:")
+if Answer.strip().lower() == "budapest" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as budapest.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than budapest.
+
+
+#Assigning the eigth question to the variable.
+Answer = input("What is the capital of Moldova?:")
+if Answer.strip().lower() == "chisinau" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as Chisinau.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than Chisinau.
+
+
+#Assigning the nineth question to the variable.
+Answer = input("What is the capital of Denmark?:")
+if Answer.strip().lower() == "copenhagen" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as Copenhagen.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than Copenhagen.
+
+
+#Assigning the tenth question to the variable.
+Answer = input("What is the capital of Finland?:")
+if Answer.strip().lower() == "helsinki" : #Selecting the answer of the question.
+    print("The answer is correct.") # printing "The answer is correct" if the answer is given as Helsinki.
+else:
+    print("The answer is wrong.")#printing "The answer is wrong" if anything else is written other than Helsinki.
