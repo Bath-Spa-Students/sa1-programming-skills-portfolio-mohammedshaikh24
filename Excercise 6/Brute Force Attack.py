c3b7b11db61b7f6c394e77059c34d33f6d5b7c0e
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-14 09:18:35 +0400

    Brute Force Attack

--- Excercise 6/Brute Force Attack.py
+++ Excercise 6/Brute Force Attack.py
@@ -1,3 +1,11 @@
+#Write a program that simulates a password entry system. The correct password is defined as 12345. The program should keep asking the user to enter the password until they provide the correct one.
+
+### Basic Requirements:
+#1. Define the correct password.
+#2. Use a while loop to repeatedly ask the user for the password until the correct one is entered.
+#3. Output an appropriate message when the correct password is entered.
+
+
 def password_for_user_verification_system():#asks the user for a password and checks if it is the right password
     real_password = "12345"# The right password for user verification
     
