161b534341d471eb03f22b0098398200cacf616e
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-16 10:25:42 +0400

    Hello

---
+++ Excercise 9/Hello.py
@@ -0,0 +1,11 @@
+#Fill in the blanks in the code below so that the function hello() prints "Hello" to the console.
+#```python
+
+def hello():
+    print("Hello")#print hello to the output area
+
+def main():
+    hello()#call the hello function to print its output
+
+if __name__ == "__main__":#this prompt makes sure it runs main() only when the you run the file
+    main()
