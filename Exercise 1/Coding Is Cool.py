8083303429328e1bba9c59a27c47495f3cf222f9
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:08:01 +0400

    Coding is cool

--- Exercise 1/Coding Is Cool.py
+++ Exercise 1/Coding Is Cool.py
@@ -1,12 +1,12 @@
-#Selecting the word1 to be coding
-word1 = "coding"
+#Selecting the variable "kitkat" to be I
+kitkat = "I"
 
-#Selecting the word2 to be is
-word2 = "is"
+#Selecting the variable "mnr" to be Love
+mnr = "Love"
 
 #Selecting the word3 to be cool
-word3 = "cool"
+timtim = "Mnr"
 
 #combining all the words
-c = word1 + " " + word2 + " " + word3
-print (c)
+jan = kitkat + " " + mnr + " " + timtim
+print (jan)
