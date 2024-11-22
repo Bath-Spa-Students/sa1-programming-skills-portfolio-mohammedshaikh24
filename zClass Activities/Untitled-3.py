671e89786b08b6857adab4ec5d47ab07bc140cc4
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:30:51 +0400

    Untitled-3

---
+++ zClass Activities/Untitled-3.py
@@ -0,0 +1,14 @@
+#performing calculations
+print(4+ 5)
+print(10-6)
+print(20 *3)
+print (40/2) #for floating point
+print (60//2) #for integer
+
+a = 250
+b = 300 #values assigned to variables
+if b > a :
+  print ("b is greater than a")
+else :
+  print ("a is greater than b")
+
