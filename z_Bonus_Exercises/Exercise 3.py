ea23a77afdc9c409575fa25d47f68ed0c7c88b82
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:11:58 +0400

    Exercise 3

---
+++ z_Bonus_Exercises/Exercise 3.py
@@ -0,0 +1,9 @@
+#putting your favourite integer in a variable
+mnr_integer = 26
+
+#print a message saying this is your most liked integer
+output = f"My most liked number is {mnr_integer}"
+
+#this prompt prints the message 
+print(output)
+
