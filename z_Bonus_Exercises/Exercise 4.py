7f115647f364180b8df81dfc273219df2a0903ad
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:22:31 +0400

    Exercise 4

---
+++ z_Bonus_Exercises/Exercise 4.py
@@ -0,0 +1,24 @@
+#make a list of a few names of your own friends
+mnr = ["Tim" , "kitkat" , "ivan" , "arman" , "faris" , "ali" , "fateen"]
+
+#print all of the names of your friends in the list one by one by using element
+#print the first name of your friend on the list
+print(mnr[0])
+
+#print the second name of your friend on the list
+print(mnr[1])
+
+#print the third name of your friend on the list
+print(mnr[2])
+
+#print the fourth name of your friend on the list
+print(mnr[3])
+
+#print the fifth name of your friend on the list
+print(mnr[4])
+
+#print the sixth name of your friend on the list
+print(mnr[5])
+
+#print the first name of your friend on the list
+print(mnr[6])
