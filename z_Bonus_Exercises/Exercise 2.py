87f36012039b67ed4a48fdf44c9f63088f725f4e
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:10:36 +0400

    Exercise 2

---
+++ z_Bonus_Exercises/Exercise 2.py
@@ -0,0 +1,7 @@
+#assigning a message to a variable
+manaar = "Hi,I love cats."
+print(manaar)#prompt to print the message
+
+#assigning a new message to the same variable 
+manaar = "Hi, My name is mohammed and I love cats and mnr."
+print(manaar)#this prompt will print the new message
