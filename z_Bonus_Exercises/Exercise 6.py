688b7eac7a8e546c64c05d537fefaf1e66d02b33
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:26:32 +0400

    Exercise 6

---
+++ z_Bonus_Exercises/Exercise 6.py
@@ -0,0 +1,8 @@
+#assigning a variable
+person_info = {"first" : "manaar" ,
+               "last" : "shaikh" ,
+               "age" : "18",
+               "state": "dubai"}
+
+#adding /n to every answer so that it appears in a new line
+print(f"First Name: {person_info['first']}\nLast Name: {person_info['last']}\nAge: {person_info['age']}\nState: {person_info['state']}")
