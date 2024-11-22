b2c5a3820912922c63ab8800537a9981b37d427f
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:31:59 +0400

    Untitled-2

---
+++ zClass Activities/Untitled-2.py
@@ -0,0 +1,36 @@
+FATHERNAME= "SIKANDER"
+NAME = "mohammed"
+AGE = "18"
+
+#string concenatation through variable
+print(FATHERNAME,NAME,AGE) 
+message = 'bath' + 'spa'
+print (message)
+
+#string concat through print statement
+print('kratos of war ' +
+ 'aterus of faye ' +
+  'cats ' )
+
+#string concat through different variables 
+a = "bath"
+b = "spa"
+c = a + " " + b
+print(c)
+
+#string concat through different variablesa 
+a = "aint no"
+b = "party like a"
+c = "diddy party"
+d = a + " " + b + " " + c
+print(d)
+
+#To get input from user
+first_name = input ("Enter your name:")
+print(first_name)
+
+Last_name = input ("Enter your last name:")
+print (Last_name)
+
+age = float(input ("Enter your age:"))
+print(age)
