9aa96d5d183c05e3b3a89fabad7cea469b865260
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:30:24 +0400

    nested

---
+++ zClass Activities/nested.py
@@ -0,0 +1,9 @@
+salary = int (input("Enter your salary:"))
+if salary >= 30000:
+    year_on_job = float (input("Enter the years of job: "))
+    if year_on_job >= 2:
+      print("You qualify for your job")
+    else :
+      print("experience is less than 2")
+else:print ("you need to earn atleast 30k to qualify")
+      
