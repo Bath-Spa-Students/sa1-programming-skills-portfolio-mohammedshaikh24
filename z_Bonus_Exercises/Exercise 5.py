fa9c473044a1271aa98a450de8c7766b859d5d26
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:26:10 +0400

    Exercise 5

---
+++ z_Bonus_Exercises/Exercise 5.py
@@ -0,0 +1,9 @@
+
+
+
+#select the color of the alien 
+alien_colour = "yellow"
+
+#Selecting the answer of the question.
+if alien_colour == "green" :
+   print("The answer is correct, You have obtained 5 points!!!.") # printing "The answer is correct, You have obtained 5 points!!!." if the answer is given as green.
