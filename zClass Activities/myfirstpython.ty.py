73f174c4cd46f73bd4f082e542d57d7d4e985abc
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:30:17 +0400

    myfirstpython

---
+++ zClass Activities/myfirstpython.ty.py
@@ -0,0 +1,11 @@
+
+print("welcome to code lab 1")
+print("""twinkle star 
+      yes bro""")
+"""
+This is a comment
+written in 
+more than just one line
+"""
+print("""ba ba black sheep
+   have you any wool?"""  )
