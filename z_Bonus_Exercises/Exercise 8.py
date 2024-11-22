a5f63b247363c28c9715c4b9b02adf47368bcc22
mohammedshaikh24 <mohammedsikandershaikh@gmail.com>
2024-11-22 11:27:05 +0400

    Exercise 8

---
+++ z_Bonus_Exercises/Exercise 8.py
@@ -0,0 +1,4 @@
+#the function is defined as favorite_book
+def favorite_book(mnr):
+    print(f"The book that I love one of the most is called {mnr}")
+favorite_book("Resident evil 7")#running the function with a book name of your choice
